5f2585f0dda7df6e17c99d75f32ecfee03404923
Matt Butcher <mbutcher@engineyard.com>
2015-09-01 13:09:58 -0600

    feat(deisctl): add trireme support for config

--- trireme/main.go
+++ trireme/main.go
@@ -5,9 +5,33 @@ import (
 	"os"
 
 	"github.com/codegangsta/cli"
+	"github.com/deis/deis/trireme/storage"
 )
 
+var parts = map[string]string{
+	// In alpha order, please.
+	"builder":    "the builder",
+	"controller": "the controller",
+	"database":   "the database",
+	"router":     "the router mesh",
+	"store":      "the store and all of its components",
+	// FIXME: Do the rest.
+}
+
+var config storage.Storer
+
 func main() {
+
+	// This is temporary: We need a place to store configuration data in
+	// lieu of etcd. For now, we'll store it locally in a configuration
+	// file.
+	var err error
+	config, err = storage.New(defaultConfigFile())
+	if err != nil {
+		fmt.Printf("Failed to load or create file %s", defaultConfigFile())
+		os.Exit(321)
+	}
+
 	app := cli.NewApp()
 	app.Name = "trireme"
 	app.Usage = "A control tool for Deis"
@@ -27,19 +51,126 @@ func commands() []cli.Command {
 			*/
 			Subcommands: installCommands(),
 		},
+		{
+			Name:        "config",
+			Usage:       "Get and set configuration values",
+			Subcommands: configCommands(),
+		},
 	}
 }
 
-func installCommands() []cli.Command {
-	parts := map[string]string{
-		// In alpha order, please.
-		"builder":    "the builder",
-		"controller": "the controller",
-		"database":   "the database",
-		"router":     "the router mesh",
-		"store":      "the store and all of its components",
-		// FIXME: Do the rest.
+func defaultConfigFile() string {
+	return os.ExpandEnv("${HOME}/.trireme")
+}
+
+func configCommands() []cli.Command {
+	cmds := make([]cli.Command, 0, len(parts)+3)
+
+	// Why deprecate `deisctl config <target> set ...`? Three reasons:
+	// 1. The predominant form of multi-commands is <CMD> <VERB> <DO>..., not
+	//    <CMD> <NOUN> <DO> <VERB>...
+	// 2. The common unix paradigm for subcommands is to move all variables to
+	//    the arguments portion: <CMD> <SUBCMD> <ARG1>..., not <CMD> <SUBCMD> <ARG1> <SUBCMD> <ARG2>...
+	// 3. The logic is simply cleaner when arguments are grouped together.
+	cmds = append(cmds,
+		cli.Command{
+			Name:  "get",
+			Usage: "Get an existing parameter from an existing component",
+			Action: func(c *cli.Context) {
+				a := c.Args()
+				if len(a) < 2 {
+					fmt.Println("Usage: deisctl config get <TARGET> <KEY>")
+					os.Exit(1)
+				}
+				ns := a[0]
+				key := a[1]
+				val, err := config.Get(ns, key)
+				if err != nil {
+					fmt.Printf("Failed to get %s:%s: '%s'\n", ns, key, err)
+					os.Exit(2)
+				}
+				fmt.Println(val)
+			},
+		},
+		cli.Command{
+			Name:  "set",
+			Usage: "Set an existing parameter for an existing component",
+			Action: func(c *cli.Context) {
+				a := c.Args()
+				if len(a) < 3 {
+					fmt.Println("Usage: deisctl config get <TARGET> <KEY> <VALUE>")
+					os.Exit(1)
+				}
+				ns := a[0]
+				key := a[1]
+				val := a[2]
+				if err := config.Set(ns, key, val); err != nil {
+					fmt.Printf("Failed to set %s:%s=%s: '%s'\n", ns, key, val, err)
+					os.Exit(3)
+				}
+				fmt.Println(val)
+			},
+		},
+		cli.Command{
+			Name:  "rm",
+			Usage: "Remove an existing parameter from an existing component",
+			Action: func(c *cli.Context) {
+				a := c.Args()
+				if len(a) < 2 {
+					fmt.Println("Usage: deisctl config rm <TARGET> <KEY>")
+					os.Exit(1)
+				}
+				ns := a[0]
+				key := a[1]
+				if err := config.Remove(ns, key); err != nil {
+					fmt.Printf("Failed to remove %s:%s: '%s'\n", ns, key, err)
+					os.Exit(4)
+				}
+			},
+		},
+	)
+
+	for n, _ := range parts {
+		cmds = append(cmds, cli.Command{
+			Name:  n,
+			Usage: fmt.Sprintf("Deprecated. Use `deisctl config set|get|rm %s`", n),
+			Action: func(c *cli.Context) {
+				a := c.Args()
+				if len(a) < 2 {
+					fmt.Println("Usage: deisctl config get|set|rm <TARGET> <KEY> [<VALUE>]")
+					os.Exit(1)
+				}
+				key := a[1]
+				switch a[0] {
+				case "get":
+					val, err := config.Get(n, key)
+					if err != nil {
+						fmt.Println(err)
+						os.Exit(2)
+					}
+					fmt.Println(val)
+				case "set":
+					if err := config.Set(n, key, a[2]); err != nil {
+						fmt.Println("Usage: deisctl config get|set|rm <TARGET> <KEY> [<VALUE>]")
+						os.Exit(3)
+					}
+					fmt.Println(a[2])
+				case "rm":
+					if err := config.Remove(n, key); err != nil {
+						fmt.Println("Usage: deisctl config get|set|rm <TARGET> <KEY> [<VALUE>]")
+						os.Exit(4)
+					}
+				default:
+					fmt.Println("Usage: deisctl config get|set|rm <TARGET> <KEY> [<VALUE>]")
+					os.Exit(1)
+				}
+			},
+		})
 	}
+	return cmds
+}
+
+func installCommands() []cli.Command {
 
 	// This basically ensures that append() will not have to reallocate.
 	cmds := make([]cli.Command, 0, len(parts)+1)
