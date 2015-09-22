f50cc6c732f1c30865e46e8612ef9d21d805dfb1
Nathan Smith <nathansmith22@gmail.com>
2015-09-22 18:17:38 -0400

    logs stopped working

--- logger/syslogd/syslogd.go
+++ logger/syslogd/syslogd.go
@@ -90,13 +90,13 @@ func (h *handler) mainLoop() {
 			break
 		}
 		drainURI := h.drainURI
-		re := regexp.MustCompile("no-drain")
-		match := re.FindStringIndex(m.String())
-		if match == nil {
-			log.Println("no-drain Drain URI is " + drainURI)
-		}
+		//re := regexp.MustCompile("no-drain")
+		//match := re.FindStringIndex(m.String())
+		//if match == nil {
+			//log.Println("no-drain Drain URI is " + drainURI)
+		//}
 		if h.drainURI != "" {
-			drain.SendToDrain(m.String(), h.drainURI)
+			drain.SendToDrain(m.String(), drainURI)
 		}
 		err := writeToDisk(m)
 		if err != nil {
